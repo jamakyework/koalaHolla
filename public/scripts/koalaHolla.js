6f44cfc0496fc248cbbada5f157a2baa01f35155
James Ericson <jamesericson@gmail.com>
2016-12-14 17:59:03 -0600

    able to display koalas and add

--- public/scripts/koalaHolla.js
+++ public/scripts/koalaHolla.js
@@ -12,11 +12,11 @@ $( document ).ready( function(){
     // NOT WORKING YET :(
     // using a test object
     var objectToSend = {
-      name: 'testName',
-      age: 'testName',
-      sex: 'testName',
-      readyForTransfer: 'testName',
-      notes: 'testName',
+      name: $('#nameIn').val(),
+      age: $('#ageIn').val(),
+      sex: $('#sexIn').val(),
+      ready_for_transfer: $('#readyForTransferIn').val(),
+      notes: $('#notesIn').val(),
     };
     // call saveKoala with the new obejct
     saveKoala( objectToSend );
@@ -31,6 +31,7 @@ var getKoalas = function(){
     type: 'GET',
     success: function( data ){
       console.log( 'got some koalas: ', data );
+      displaySomeKoalas( data );
     } // end success
   }); //end ajax
   // display on DOM with buttons that allow edit of each
@@ -45,6 +46,17 @@ var saveKoala = function( newKoala ){
     data: newKoala,
     success: function( data ){
       console.log( 'got some koalas: ', data );
+      displaySomeKoalas( data );
+
     } // end success
   }); //end ajax
 }
+
+var displaySomeKoalas = function( koalaArray ){
+  var outputText = '';
+  $('#outputKoalas').html('');
+  for (var i = 0; i < koalaArray.length; i++) {
+    $('#outputKoalas').append('<tr><td>' + koalaArray[i].name + '</td> <td>' + koalaArray[i].age + '</td> <td>' + koalaArray[i].sex + '</td> <td>' + koalaArray[i].ready_for_transfer + '</td> <td>' + koalaArray[i].notes + '</td></tr>');
+    // <tr><td>koalaArray[i].name</td> <td>koalaArray[i].age</td> <td>koalaArray[i].sex</td> <td>koalaArray[i].ready_for_transfer</td> <td>koalaArray[i].notes</td></tr>
+  }
+}; // end displaySomeKoalas()
